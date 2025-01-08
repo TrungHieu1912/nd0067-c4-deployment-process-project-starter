39470ca1c13295132d0c0e1ef31ca24989071139
TrungHieu1912 <52956674+TrungHieu1912@users.noreply.github.com>
2025-01-08 15:19:51 +0700

    Update set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=fnmMekGELEESQtMrjhoa
-export POSTGRES_HOST=database-1.cz3ulvi4oilf.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.c2hsqc4kwiv2.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export POSTGRES_PORT=5432
-export AWS_BUCKET=arn:aws:s3:::hungvdh-udagram-bucket
+export AWS_BUCKET=arn:aws:s3:::hieult31-udagram-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
